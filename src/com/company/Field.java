01ec96c4e3fcd7ac4ec6d6fc54ced4b4092ee403
Регина <rmashkovtseva@gmail.com>
2023-01-31 20:13:24 +0300

    Получилось создать поле для игры в отдельном классе. Правда после вывода массива в конце неизвестные символы намекающие на использование toString

--- src/com/company/Field.java
+++ src/com/company/Field.java
@@ -4,40 +4,19 @@ public class Field {
     private static int column = 3;
     private static int row = 3;
     private static char[][] ch = new char[row][column];
+    private static char sim = '\u005f';
 
 
     public static char[][] mas() {
 
-//        for(int i = 0; i < row; i++){
-//            for(int j = 0; j < column; j++ ){
-//                ch[i][j] = '\u005f';
-//            }
-//            System.out.println(" ");
-//        }
-//        System.out.println("\n");
-//        return ch;
-//
-//
         for(int i = 0; i < row; i++){
             for(int j = 0; j < column; j++ ){
-                ch[i][j] = '\u005f';
+                System.out.print(ch[i][j] = sim);
+                System.out.print(" ");
             }
             System.out.println(" ");
         }
-        System.out.println("\n");
         return ch;
-
-
-
-
-//        String s1 = "First String";
-//        char[][] charArray = s1.toCharArray();
-//        for (char c : charArray) {
-//            System.out.print(" " + c);
-//        }
-
     }
-
-
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,10 @@
 package com.company;
 
 public class Main {
-    private static int row = 3;
-    private static int column = 3;
-    private static String str = " _ ";
-
-    private static String[][] ch = new String[row][column];
-
 
     public static void main(String[] args) {
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < column; j++ ){
-                System.out.print(ch[i][j] = str);
-            }
-            System.out.println(" ");
-        }
+
+        System.out.println((Field.mas()));
+
     }
 }
